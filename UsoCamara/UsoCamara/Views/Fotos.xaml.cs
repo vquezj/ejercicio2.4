2ba0feed37830803b3c9cf4a44f26ef0a8de2c31
Jessy Velasquez <jessyvelasquez3006@gmail.com>
2021-11-02 22:26:33 -0600

    ejercicio2.4

---
+++ UsoCamara/UsoCamara/Views/Fotos.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UsoCamara.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Fotos : ContentPage
+    {
+        public Fotos()
+        {
+            InitializeComponent();
+            this.btnFoto.Clicked += Btnfoto_Clicked;
+        }
+
+        private async void Btnfoto_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+                if(!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No hay camara", "No se detecta la camara", "Ok");
+                    return;
+                }
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    //variable para guardar la variable en el album publico
+                    SaveToAlbum = true
+                });
+                if (file == null)
+                    return;
+
+                this.imgcamara.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+
+                await DisplayAlert("Foto Capturada", "Localizacion: " + file.AlbumPath, "Ok");
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Permiso denegado", "De los permisos de camara al dispositivo. \nError: " + ex.Message, "Ok");
+            }
+        }
+    }
+}
